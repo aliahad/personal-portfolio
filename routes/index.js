cb69513dddf054300d475a5edbf95d46f4d75193
Ali Ahad Badruddin <alyhad.jariya@gmail.com>
2021-11-23 00:00:56 -0500

    Added home, about, projects and contact pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page' });
+});
+
+/* GET about me page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me Page' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'My Projects Page' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me Page' });
 });
 
 module.exports = router;
